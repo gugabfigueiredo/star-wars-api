c1c7b25c37cf726b3a5f03d16ab84c527087e7db
Guga Figueiredo <gugabfigueiredo@gmail.com>
2021-09-01 08:30:56 -0300

    added planet model and utils

--- model/planets.go
+++ model/planets.go
@@ -1,9 +1,13 @@
 package model
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Planet struct {
-	ID int64
-	Name string
-	Weather string
-	Terrain	string
-	Appearences int
+	ID primitive.ObjectID 	`bson:"_id,omitempty"`
+	Name string				`bson:"name,omitempty"`
+	Weather string			`bson:"weather,omitempty"`
+	Terrain	string			`bson:"terrain,omitempty"`
+	Refs int				`bson:"references,omitempty"`
 }
---
+++ model/utils.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"github.com/gugabfigueiredo/swapi"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func WritePlanetModel(planet swapi.Planet) mongo.WriteModel {
+	model := mongo.NewUpdateOneModel()
+	model.SetFilter(bson.M{"name": planet.Name})
+	model.SetUpdate(bson.M{
+		"name": planet.Name,
+		"weather": planet.Climate,
+		"terrain": planet.Terrain,
+		"references": len(planet.FilmURLs),
+	})
+	model.SetUpsert(true)
+	return model
+}
