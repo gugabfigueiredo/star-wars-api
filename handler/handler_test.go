e5426e5bc31e42cbc935e95f0c72161808316198
Guga Figueiredo <gugabfigueiredo@gmail.com>
2021-09-05 21:41:50 -0300

    added testing for update and delete

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -10,6 +11,7 @@ import (
 	"github.com/gugabfigueiredo/star-wars-api/test"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -30,20 +32,20 @@ func TestAPIHandler_FindPlanets(t *testing.T) {
 	}{
 		{
 			name: "find planet by name",
-			stub: &test.Stub{Planet: &model.Planet{Name: "Planet", Weather: "nice", Terrain: "rocky", Refs: 0}, RespBody: &model.Planet{}},
+			stub: &test.Stub{Planet: &model.Planet{Name: "Planet", Climate: "nice", Terrain: "rocky", Refs: 0}, RespBody: &model.Planet{}},
 			endpoint: "name",
 			pathParam: "name",
-			expectedBody: &model.Planet{Name: "Planet", Weather: "nice", Terrain: "rocky", Refs: 0},
+			expectedBody: &model.Planet{Name: "Planet", Climate: "nice", Terrain: "rocky", Refs: 0},
 			expectedStatusCode: http.StatusOK,
 			expectedContentType: "application/json",
 			expectedCalledWith: map[string]interface{}{"filter": bson.M{"name": "name"}},
 		},
 		{
 			name: "find planet by id",
-			stub: &test.Stub{Planet: &model.Planet{Name: "Planet", Weather: "nice", Terrain: "rocky", Refs: 0}, RespBody: &model.Planet{}},
+			stub: &test.Stub{Planet: &model.Planet{Name: "Planet", Climate: "nice", Terrain: "rocky", Refs: 0}, RespBody: &model.Planet{}},
 			endpoint: "id",
 			pathParam: "1234",
-			expectedBody: &model.Planet{Name: "Planet", Weather: "nice", Terrain: "rocky", Refs: 0},
+			expectedBody: &model.Planet{Name: "Planet", Climate: "nice", Terrain: "rocky", Refs: 0},
 			expectedStatusCode: http.StatusOK,
 			expectedContentType: "application/json",
 			expectedCalledWith: map[string]interface{}{"filter": bson.M{"_id": "1234"}},
@@ -88,19 +90,19 @@ func TestAPIHandler_FindPlanets(t *testing.T) {
 			name: "get all planets",
 			stub: &test.Stub{
 				Planets: []*model.Planet{
-					{Name: "Planet1", Weather: "nice", Terrain: "rocky", Refs: 1},
-					{Name: "Planet2", Weather: "warm", Terrain: "icy", Refs: 2},
-					{Name: "Planet3", Weather: "cold", Terrain: "plains", Refs: 3},
-					{Name: "Planet4", Weather: "nice", Terrain: "forest", Refs: 4},
+					{Name: "Planet1", Climate: "nice", Terrain: "rocky", Refs: 1},
+					{Name: "Planet2", Climate: "warm", Terrain: "icy", Refs: 2},
+					{Name: "Planet3", Climate: "cold", Terrain: "plains", Refs: 3},
+					{Name: "Planet4", Climate: "nice", Terrain: "forest", Refs: 4},
 				},
 				RespBody: &[]*model.Planet{},
 			},
 			endpoint: "planets",
 			expectedBody: &[]*model.Planet{
-				{Name: "Planet1", Weather: "nice", Terrain: "rocky", Refs: 1},
-				{Name: "Planet2", Weather: "warm", Terrain: "icy", Refs: 2},
-				{Name: "Planet3", Weather: "cold", Terrain: "plains", Refs: 3},
-				{Name: "Planet4", Weather: "nice", Terrain: "forest", Refs: 4},
+				{Name: "Planet1", Climate: "nice", Terrain: "rocky", Refs: 1},
+				{Name: "Planet2", Climate: "warm", Terrain: "icy", Refs: 2},
+				{Name: "Planet3", Climate: "cold", Terrain: "plains", Refs: 3},
+				{Name: "Planet4", Climate: "nice", Terrain: "forest", Refs: 4},
 			},
 			expectedStatusCode: http.StatusOK,
 			expectedContentType: "application/json",
@@ -122,7 +124,6 @@ func TestAPIHandler_FindPlanets(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.stub.Logger = logger
 			h := &APIHandler{
 				IService: tt.stub,
 				Logger:   logger,
@@ -164,12 +165,12 @@ func TestAPIHandler_FindPlanets(t *testing.T) {
 	}
 }
 
-func TestAPIHandler_CreatePlanets(t *testing.T) {
+func TestAPIHandler_CreateUpdateDelete(t *testing.T) {
 
 	tests := []struct{
 		name               	string
 		stub               	*test.Stub
-		requestBody        	map[string]interface{}
+		requestBody        	interface{}
 		endpoint			string
 		expectedStatusCode 	int
 		expectedBody       	interface{}
@@ -179,13 +180,181 @@ func TestAPIHandler_CreatePlanets(t *testing.T) {
 	}{
 		{
 			name: "create one planet",
-			requestBody: map[string]interface{}{"planets": []*model.Planet{
-				{Name: "NewPlanet", Weather: "nice", Terrain: "slimy", Refs: 0},
-			}},
+			stub: &test.Stub{
+				InsertResult: mongo.InsertManyResult{InsertedIDs: []interface{}{0}},
+				RespBody: &mongo.InsertManyResult{},
+			},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			},
+			endpoint: "create",
 			expectedStatusCode: http.StatusOK,
+			expectedContentType: "application/json",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			}},
+			expectedBody: &mongo.InsertManyResult{InsertedIDs: []interface{}{0}},
 		},
-		{name: "create many planets"},
-		{name: "fail to create"},
+		{
+			name: "create many planets",
+			stub: &test.Stub{
+				InsertResult: mongo.InsertManyResult{InsertedIDs: []interface{}{0,1,2}},
+				RespBody: &mongo.InsertManyResult{},
+			},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+				{Name: "NewPlanet", Climate: "warm", Terrain: "slimy", Refs: 1},
+				{Name: "NewPlanet", Climate: "cold", Terrain: "slimy", Refs: 2},
+			},
+			endpoint: "create",
+			expectedStatusCode: http.StatusOK,
+			expectedContentType: "application/json",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+				{Name: "NewPlanet", Climate: "warm", Terrain: "slimy", Refs: 1},
+				{Name: "NewPlanet", Climate: "cold", Terrain: "slimy", Refs: 2},
+			}},
+			expectedBody: &mongo.InsertManyResult{InsertedIDs: []interface{}{0,1,2}},
+		},
+		{
+			name: "fail to decode request body",
+			stub: &test.Stub{RespBody: &mongo.InsertManyResult{}},
+			requestBody: []map[string]interface{}{{"name": 1}},
+			endpoint: "create",
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedContentType: "text/plain; charset=utf-8",
+		},
+		{
+			name: "fail to create planet",
+			stub: &test.Stub{RespBody: &mongo.InsertManyResult{}, Error: errors.New("failed to insert planets")},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			},
+			endpoint: "create",
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedContentType: "text/plain; charset=utf-8",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			}},
+		},
+		{
+			name: "update one planet",
+			stub: &test.Stub{
+				UpdateResult: mongo.BulkWriteResult{MatchedCount: 1, ModifiedCount: 1},
+				RespBody: &mongo.BulkWriteResult{},
+			},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			},
+			endpoint: "update",
+			expectedStatusCode: http.StatusOK,
+			expectedContentType: "application/json",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			}},
+			expectedBody: &mongo.BulkWriteResult{MatchedCount: 1, ModifiedCount: 1},
+		},
+		{
+			name: "update many planets",
+			stub: &test.Stub{
+				UpdateResult: mongo.BulkWriteResult{MatchedCount: 3, ModifiedCount: 3},
+				RespBody: &mongo.BulkWriteResult{},
+			},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+				{Name: "NewPlanet", Climate: "warm", Terrain: "slimy", Refs: 1},
+				{Name: "NewPlanet", Climate: "cold", Terrain: "slimy", Refs: 2},
+			},
+			endpoint: "update",
+			expectedStatusCode: http.StatusOK,
+			expectedContentType: "application/json",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+				{Name: "NewPlanet", Climate: "warm", Terrain: "slimy", Refs: 1},
+				{Name: "NewPlanet", Climate: "cold", Terrain: "slimy", Refs: 2},
+			}},
+			expectedBody: &mongo.BulkWriteResult{MatchedCount: 3, ModifiedCount: 3},
+		},
+		{
+			name: "fail to decode request body",
+			stub: &test.Stub{RespBody: &mongo.BulkWriteResult{}},
+			requestBody: []map[string]interface{}{{"name": 1}},
+			endpoint: "update",
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedContentType: "text/plain; charset=utf-8",
+		},
+		{
+			name: "fail to update planets",
+			stub: &test.Stub{RespBody: &mongo.BulkWriteResult{}, Error: errors.New("failed to update planets")},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			},
+			endpoint: "update",
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedContentType: "text/plain; charset=utf-8",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			}},
+		},
+		{
+			name: "delete one planet",
+			stub: &test.Stub{
+				DeleteResult: mongo.DeleteResult{DeletedCount: 1},
+				RespBody: &mongo.DeleteResult{},
+			},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			},
+			endpoint: "delete",
+			expectedStatusCode: http.StatusOK,
+			expectedContentType: "application/json",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			}},
+			expectedBody: &mongo.DeleteResult{DeletedCount: 1},
+		},
+		{
+			name: "delete many planets",
+			stub: &test.Stub{
+				DeleteResult: mongo.DeleteResult{DeletedCount: 3},
+				RespBody: &mongo.DeleteResult{},
+			},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+				{Name: "NewPlanet", Climate: "warm", Terrain: "slimy", Refs: 1},
+				{Name: "NewPlanet", Climate: "cold", Terrain: "slimy", Refs: 2},
+			},
+			endpoint: "delete",
+			expectedStatusCode: http.StatusOK,
+			expectedContentType: "application/json",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+				{Name: "NewPlanet", Climate: "warm", Terrain: "slimy", Refs: 1},
+				{Name: "NewPlanet", Climate: "cold", Terrain: "slimy", Refs: 2},
+			}},
+			expectedBody: &mongo.DeleteResult{DeletedCount: 3},
+		},
+		{
+			name: "fail to decode request body",
+			stub: &test.Stub{RespBody: &mongo.DeleteResult{}},
+			requestBody: []map[string]interface{}{{"name": 1}},
+			endpoint: "delete",
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedContentType: "text/plain; charset=utf-8",
+		},
+		{
+			name: "fail to create planet",
+			stub: &test.Stub{RespBody: &mongo.DeleteResult{}, Error: errors.New("failed to insert planets")},
+			requestBody: []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			},
+			endpoint: "delete",
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedContentType: "text/plain; charset=utf-8",
+			expectedCalledWith: map[string]interface{}{"planets": []model.Planet{
+				{Name: "NewPlanet", Climate: "nice", Terrain: "slimy", Refs: 0},
+			}},
+		},
 	}
 
 	logger := log.New(&log.Config{
@@ -196,22 +365,28 @@ func TestAPIHandler_CreatePlanets(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.stub.Logger = logger
 			h := &APIHandler{
 				IService: tt.stub,
 				Logger:   logger,
 			}
 
 			router := chi.NewRouter()
-			router.Get("/create", h.CreatePlanets)
+			router.Post("/create", h.CreatePlanets)
+			router.Post("/update", h.PlanetUpdate)
+			router.Post("/delete", h.RemovePlanets)
 			mockServer := httptest.NewServer(router)
 			defer mockServer.Close()
 
-			endpoint := fmt.Sprintf("%s/%s", mockServer.URL, tt.endpoint)
-			if tt.pathParam != "" {
-				endpoint = fmt.Sprintf("%s/%s", endpoint, tt.pathParam)
+			b, err := json.Marshal(map[string]interface{}{})
+			if tt.requestBody != nil {
+				b, err = json.Marshal(tt.requestBody)
 			}
-			r, err := http.NewRequest(http.MethodGet, endpoint, nil)
+			if err != nil {
+				t.Fatalf("could not marshal request body")
+			}
+
+			endpoint := fmt.Sprintf("%s/%s", mockServer.URL, tt.endpoint)
+			r, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewReader(b))
 			if err != nil {
 				t.Fatalf("could not create request. err %+v\n", err)
 			}
@@ -229,7 +404,7 @@ func TestAPIHandler_CreatePlanets(t *testing.T) {
 						t.Fatalf("could not read the response body. err %+v\n", err)
 					}
 
-					assert.Equal(t, tt.expectedBody, tt.stub.RespBody)
+					assert.Equal(t, test.AsString(tt.expectedBody), test.AsString(tt.stub.RespBody))
 				}
 			}
 		})
@@ -241,16 +416,55 @@ func TestAPIHandler_UpdatePlanetRefs(t *testing.T) {
 	tests := []struct{
 		name               	string
 		stub               	*test.Stub
-		requestBody        	map[string]interface{}
-		endpoint			string
-		pathParam			string
 		expectedStatusCode 	int
-		expectedBody       	interface{}
 		expectedErr        	error
-		expectedCalledWith 	map[string]interface{}
 		expectedContentType	string
 	}{
-		{name: "update planet refs"},
-		{name: "fail to update planet refs"},
+		{
+			name: "update planet refs",
+			stub: &test.Stub{},
+			expectedStatusCode: http.StatusOK,
+			expectedContentType: "application/json",
+		},
+		{
+			name: "fail to get updated refs",
+			stub: &test.Stub{Error: errors.New("failed to get updated refs")},
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedContentType: "text/plain; charset=utf-8",
+
+		},
+	}
+
+	logger := log.New(&log.Config{
+		Context:               "sw-api-test",
+		ConsoleLoggingEnabled: false,
+		EncodeLogsAsJson:      true,
+	})
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &APIHandler{
+				IService: tt.stub,
+				Logger:   logger,
+			}
+
+			router := chi.NewRouter()
+			router.Get("/", h.SetMovieRefs)
+			mockServer := httptest.NewServer(router)
+			defer mockServer.Close()
+
+			r, err := http.NewRequest(http.MethodGet, mockServer.URL, nil)
+			if err != nil {
+				t.Fatalf("could not create request. err %+v\n", err)
+			}
+
+			resp, err := http.DefaultClient.Do(r)
+			assert.Equal(t, test.AsString(tt.expectedErr), test.AsString(err))
+			if err == nil {
+				defer resp.Body.Close()
+				assert.Equal(t, tt.expectedContentType, resp.Header.Get("Content-Type"))
+				assert.Equal(t, tt.expectedStatusCode, resp.StatusCode)
+			}
+		})
 	}
 }
