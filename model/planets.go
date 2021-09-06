23dab908306f2df158415c352d926f273cad916a
Guga Figueiredo <gugabfigueiredo@gmail.com>
2021-09-05 21:39:43 -0300

    clarify and lint

--- model/planets.go
+++ model/planets.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Planet struct {
-	ID primitive.ObjectID 	`bson:"_id,omitempty"`
-	Name string				`bson:"name,omitempty"`
-	Weather string			`bson:"weather,omitempty"`
-	Terrain	string			`bson:"terrain,omitempty"`
-	Refs int				`bson:"references,omitempty"`
+	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	Name    string             `bson:"name,omitempty"`
+	Climate string             `bson:"weather,omitempty"`
+	Terrain string             `bson:"terrain,omitempty"`
+	Refs    int                `bson:"references,omitempty"`
 }
