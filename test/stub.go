861c1cf414d0d740507a6707e3371e012b68b5f9
Guga Figueiredo <gugabfigueiredo@gmail.com>
2021-09-05 21:42:36 -0300

    update stub to match test criteria

--- test/stub.go
+++ test/stub.go
@@ -2,27 +2,26 @@ package test
 
 import (
 	"fmt"
-	"github.com/gugabfigueiredo/star-wars-api/log"
 	"github.com/gugabfigueiredo/star-wars-api/model"
 	"github.com/gugabfigueiredo/swapi"
 	"go.mongodb.org/mongo-driver/mongo"
-	"time"
 )
 
 type StringResponse string
 
 type Stub struct {
-	Logger *log.Logger
 
 	Planet *model.Planet
 	Planets []*model.Planet
 
 	SwapiPlanets []swapi.Planet
+	SwapiUpdates int
 
 	Channel chan bool
 
-	UpdateResult mongo.BulkWriteResult
 	InsertResult mongo.InsertManyResult
+	UpdateResult  mongo.BulkWriteResult
+	DeleteResult mongo.DeleteResult
 
 	CalledWith map[string]interface{}
 	RespBody interface{}
@@ -35,36 +34,38 @@ func (s *Stub) GetPlanet(filter interface{}, m *model.Planet) error {
 	if s.Planet != nil {
 		m.Name = s.Planet.Name
 		m.Terrain = s.Planet.Terrain
-		m.Weather = s.Planet.Weather
+		m.Climate = s.Planet.Climate
 		m.Refs = s.Planet.Refs
 	}
 	return s.Error
-}
-
-func (s *Stub) AllPlanets() ([]swapi.Planet, error) {
-	return s.SwapiPlanets, s.Error
 }
 
 func (s *Stub) GetAllPlanets() ([]*model.Planet, error) {
 	return s.Planets, s.Error
 }
 
-func (s *Stub) UpdatePlanets(planets []swapi.Planet) (*mongo.BulkWriteResult, error) {
+func (s *Stub) UpdateMovieRefs(planets []swapi.Planet) (*mongo.BulkWriteResult, error) {
 	s.CalledWith = map[string]interface{}{"planets": planets}
 	return &s.UpdateResult, s.Error
 }
 
-func (s *Stub) InsertPlanets(planets []*model.Planet) (*mongo.InsertManyResult, error) {
+func (s *Stub) InsertPlanets(planets []model.Planet) (*mongo.InsertManyResult, error) {
 	s.CalledWith = map[string]interface{}{"planets": planets}
 	return &s.InsertResult, s.Error
 }
 
+func (s *Stub) UpdatePlanets(planet []model.Planet) (*mongo.BulkWriteResult, error) {
+	s.CalledWith = map[string]interface{}{"planets": planet}
+	return &s.UpdateResult, s.Error
+}
+
+func (s *Stub) DeletePlanets(planets []model.Planet) (*mongo.DeleteResult, error) {
+	s.CalledWith = map[string]interface{}{"planets": planets}
+	return &s.DeleteResult, s.Error
+}
+
 func (s *Stub) UpdatePlanetRefs() error {
 	return s.Error
-}
-func (s *Stub) SchedulePlanetUpdate(interval time.Duration) chan bool {
-	s.CalledWith = map[string]interface{}{"interval": interval}
-	return s.Channel
 }
 
 func AsString(i interface{}) string {
